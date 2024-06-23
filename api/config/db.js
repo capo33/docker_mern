a581adb77c3f78a5bff74573576afd55f77d75a1
Mohamed <s2000588@edu.bc.fi>
2024-06-23 20:33:21 +0300

    create docker compose

--- api/config/db.js
+++ api/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // DB connection
 const connect = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/docker-mern')
+    await mongoose.connect(process.env.MONGO_URI)
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error(error);
